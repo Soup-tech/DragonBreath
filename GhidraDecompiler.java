42a211d8b0c53a1d09724d73c0a5a6657f6c5cf0
soup-tech <campbellssoup21@gmail.com>
2021-05-12 12:23:23 -0400

    Format changes to GhidraDecompiler.java

--- GhidraDecompiler.java
+++ GhidraDecompiler.java
@@ -1,128 +1,120 @@
-//TODO write a description for this script
-//@author 
-//@category _NEW_
-//@keybindecompInterfaceng 
-//@menupath 
-//@toolbar 
+// @TODO Work in progress
+// This is a helper function for DragonBreath.py
+// @author Soup-tech
 
-import ghidecompResultsa.app.script.GhidecompResultsaScript;
 
-import ghidecompResultsa.app.decompiler.ClangLine;
-import ghidecompResultsa.app.decompiler.DecompInterface;
-import ghidecompResultsa.app.decompiler.DecompileResults;
-import ghidecompResultsa.app.decompiler.DecompiledFunction;
-import ghidecompResultsa.app.decompiler.PrettyPrinter;
-import ghidecompResultsa.app.decompiler.component.DecompilerUtils;
+import ghidra.app.decompiler.ClangLine;
+import ghidra.app.decompiler.DecompInterface;
+import ghidra.app.decompiler.DecompileResults;
+import ghidra.app.decompiler.DecompiledFunction;
+import ghidra.app.decompiler.PrettyPrinter;
+import ghidra.app.decompiler.component.DecompilerUtils;
 
-import ghidecompResultsa.app.util.headless.HeadlessScript;
+import ghidra.app.util.headless.HeadlessScript;
 
-import ghidecompResultsa.program.model.addecompResultsess.AddecompResultsess;
-import ghidecompResultsa.program.model.listing.Function;
-import ghidecompResultsa.program.model.listing.FunctionIterator;
-import ghidecompResultsa.program.model.listing.Listing;
-import ghidecompResultsa.program.model.pcode.HighFunction;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.listing.Function;
+import ghidra.program.model.listing.FunctionIterator;
+import ghidra.program.model.listing.Listing;
+import ghidra.program.model.pcode.HighFunction;
 
 import java.util.ArrayList;
 
-public class GhidecompResultsaDecompiler extends HeadlessScript {
+public class GhidraDecompiler extends HeadlessScript {
 
 	@Override
-	public void run() throws Exception {
+  	public void run() throws Exception {
 
-	    // Stop after this headless script
-		setHeadlessContinuationOption(HeadlessContinuationOption.ABORT);
+    	// Stop after this headless script
+    	setHeadlessContinuationOption(HeadlessContinuationOption.ABORT);
 
-	    // Get the function addecompResultsess from the script arguments
-	    String[] args = getScriptArgs();
-	    println(String.format("Array length: %d", args.length)); 
+    	// Get the function address from the script arguments
+    	String[] args = getScriptArgs();
+    	println(String.format("Array length: %d", args.length));
 
-	    if (args.length == 0) {
-	      	System.err.println("Please specify a function addecompResultsess!");
-	      	System.err.println("It's deadbeef not 0xdeadbeef");
-	      	return;
-	    }
+    	if (args.length == 0) {
+      		System.err.println("Error: Please specify a function address!");
+      		System.err.println("Remember: it's deadbeef no 0xdeadbeef");
+      		return;
+    	}
 
-	    int funcAddecompResultsess;
-	    try {
-	    	funcAddecompResultsess = Integer.parseInt(args[0], 16);
-	    }
-	    catch (NumberFormatException e) {
-	      	System.err.println(String.format("Invalid hex addecompResultsess: %s", args[0]));
-	      	return;
-	    }
-	    println(String.format("AddecompResultsess: %x", funcAddecompResultsess));
+    	int functionAddress;
+    	
+    	try {
+      		functionAddress = Integer.parseInt(args[0], 16);
+    	}
+    	catch (NumberFormatException e) {
+      		System.err.println(String.format("Invalid hex address: %s", args[0]));
+      		return;
+    	}
+   		
+   		println(String.format("Address: %x", functionAddress));
 
-	    // Create DecompInterface 
-	    DecompInterface decompInterface = new DecompInterface();
-	    println("Simplification style: " + decompInterface.getSimplificationStyle()); 
-	    println("Debug enables: " + decompInterface.debugEnabled());
+    	DecompInterface decompInt = new DecompInterface();
+    	println("Simplification style: " + decompInt.getSimplificationStyle());
+    	println("Debug enables: " + decompInt.debugEnabled());
 
-	    // GhidecompResultsaDecompiler could not find addecompResultsess
-	    Function f = this.getFunction(funcAddecompResultsess);
-	    if (f == null) {
-	    	System.err.println(String.format("Function not found at 0x%x", funcAddecompResultsess));
-	      	return;
-	    }
+    	Function f = this.getFunction(functionAddress);
+    	if (f == null) {
+      		System.err.println(String.format("Function not found at 0x%x", functionAddress));
+      		return;
+    	}
 
-	    println(String.format("Decompiling %s() at 0x%x", f.getName(), funcAddecompResultsess));
+    	println(String.format("Decompiling %s() at 0x%x", f.getName(), functionAddress));
 
-	    println("Program: " + decompInterface.openProgram(f.getProgram())); // DEBUG
+    	println("Program: " + decompInt.openProgram(f.getProgram())); 
 
-	    // Decompile with a 5-seconds timeout
-	    DecompileResults decompResults = decompInterface.decompileFunction(f, 5, null);
-	    println("Decompilation completed: " + decompResults.decompileCompleted()); // DEBUG
+    	DecompileResults decompResults = decompInt.decompileFunction(f, 5, null);
+    	println("Decompilation completed: " + decompResults.decompileCompleted()); 
 
-	    DecompiledFunction df = decompResults.getDecompiledFunction();
-	    println(df.getC());
+    	DecompiledFunction decompFunction = decompResults.getDecompiledFunction();
+    	println(decompFunction.getC());
 
-	    // Print lines prepend with addecompResultsesses
+	    // Print lines prepend with addresses
 	    PrettyPrinter pp = new PrettyPrinter(f, decompResults.getCCodeMarkup());
 	    ArrayList<ClangLine> lines = pp.getLines();
 
 	    for (ClangLine line : lines) {
-	      	long minAddecompResultsess = Long.MAX_VALUE; 
-	      	long maxAddecompResultsess = 0; 
-	      	
-	      	for (int i = 0; i < line.getNumTokens(); i++) {
-	        	
-	        	if (line.getToken(i).getMinAddecompResultsess() == null) {
-	          		continue; 
-	        	}
-	        	
-	        	long addecompResults = line.getToken(i).getMinAddecompResultsess().getOffset();
-	        	minAddecompResultsess = addecompResults < minAddecompResultsess ? addecompResults : minAddecompResultsess;
-	        	maxAddecompResultsess = addecompResults > maxAddecompResultsess ? addecompResults : maxAddecompResultsess;
+	      long minAddress = Long.MAX_VALUE; 
+	      long maxAddress = 0; 
+	      for (int i = 0; i < line.getNumTokens(); i++) {
+	    	
+	    	if (line.getToken(i).getMinAddress() == null) {
+	        	continue; 
+	        }
+	        
+	        long addr = line.getToken(i).getMinAddress().getOffset();
+	        minAddress = addr < minAddress ? addr : minAddress;
+	        maxAddress = addr > maxAddress ? addr : maxAddress;
 	      	}
-	      	
-	      	if (maxAddecompResultsess == 0) {
+	    	
+	    	if (maxAddress == 0) {
 	        	println(String.format("                      - %s", line.toString()));
 	      	} else {
-	        	println(String.format("0x%-8x 0x%-8x - %s", minAddecompResultsess, maxAddecompResultsess, line.toString()));
+	        	println(String.format("0x%-8x 0x%-8x - %s", minAddress, maxAddress, line.toString()));
 	      	}
+
 	    }
 	}
 
-	protected Function getFunction(int addecompResultsess) {
-    	// Logic from https://github.com/cea-sec/Sibyl/blob/master/ext/ghidecompResultsa/ExportFunction.java
+  	protected Function getFunction(int address) {
 
-    	Listing listing = currentProgram.getListing();
-    	FunctionIterator iter = listing.getFunctions(true);
-    	
-    	while (iter.hasNext() && !monitor.isCancelled()) {
-      		Function f = iter.next();
-      		
-      		if (f.isExternal()) {
-        		continue;
-      		}
+	    Listing listing = currentProgram.getListing();
+	    FunctionIterator iter = listing.getFunctions(true);
+	    
+	    while (iter.hasNext() && !monitor.isCancelled()) {
+	      	Function f = iter.next();
+	      	if (f.isExternal()) {
+	        	continue;
+	      	}
 
-      		AddecompResultsess entry = f.getEntryPoint();
-      		if (entry != null && entry.getOffset() == addecompResultsess) {
-        		return f;
-      		}
-    	}
-    return null;
-  
-  	}
+	      	Address entry = f.getEntryPoint();
+	      	if (entry != null && entry.getOffset() == address) {
+	        	return f;
+	    	}
+	    }
+		
+		return null;
+	}
 
 }
-
